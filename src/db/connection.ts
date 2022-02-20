f98170c5d77f2cd8d81bc71fe0237e10cc846b5c
Anthony Will Solsol Soplin <masterawss@gmail.com>
2022-02-19 19:50:06 -0500

    Change url database

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,14 +3,16 @@ import {Sequelize} from 'sequelize'
 import * as dotenv from 'dotenv';
 dotenv.config();
   
-const sequelize = new Sequelize(
-    process.env.DB_NAME!,
-    process.env.DB_USERNAME!,
-    process.env.DB_PASSWORD, 
-    {
-        dialect: 'postgres',
-        host: process.env.HOST!
-    }
-);
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME!,
+//     process.env.DB_USERNAME!,
+//     process.env.DB_PASSWORD, 
+//     {
+//         dialect: 'postgres',
+//         host: process.env.HOST!
+//     }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL!)
 
 export default sequelize
