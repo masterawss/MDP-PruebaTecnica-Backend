2c33af2850154ff07519009dcce01c1a91b99b73
Anthony Will Solsol Soplin <masterawss@gmail.com>
2022-02-19 19:53:52 -0500

    asd

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -13,6 +13,8 @@ dotenv.config();
 //     }
 // );
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!)
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {"dialectOptions": {
+    "ssl": true
+  }})
 
 export default sequelize
