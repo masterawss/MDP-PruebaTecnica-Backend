1ec33941b3f41c1365421a3e4d997a985d8b45bb
Anthony Will Solsol Soplin <masterawss@gmail.com>
2022-02-19 19:58:44 -0500

    asd

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -13,8 +13,23 @@ dotenv.config();
 //     }
 // );
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {"dialectOptions": {
-    "ssl": true
-  }})
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME!,
+    username:  process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD,
+    host: process.env.HOST!,
+    port: +process.env.PORT!,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
+  });
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL!, {"dialectOptions": {
+//     "ssl": true
+//   }})
 
 export default sequelize
